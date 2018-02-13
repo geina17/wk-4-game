1a2112c43e4562d78c105d8bb6fbe5c1a0f2660c
geina17 <33171782+geina17@users.noreply.github.com>
2018-02-12 21:00:35 -0700

    Add files via upload

---
+++ game.js
@@ -0,0 +1,20 @@
+.on(click,function() {
+	var counter + = 1;
+	var numberOptions = [3,7,1,9];
+	var targetNumber =  
+})
+
+$("#burToGuess")
+	if (counter === targetNumber){
+		alert("winner")
+		counter = 0;
+	}else if (counter>= targetNumber){
+		alert("loser")
+	};
+
+$("# " ).click(function() {
+  alert( " " );
+});
+console.log()
+
+$("#container").css(opacity:50%;);
